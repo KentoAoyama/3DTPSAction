c9f99a4bd5ec0cfbdb851b02aa7d6b85c6a175ea
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2023-04-12 22:51:06 +0900

    [add]ダッシュの処理を作成開始

---
+++ Assets/Script/Player/State/PlayerDashState.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDashState : IPlayerState
+{
+    private readonly PlayerController _player;
+
+    public PlayerDashState(PlayerController player)
+    {
+        _player = player;
+    }
+
+    public void Enter()
+    {
+
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void Exit()
+    {
+
+    }
+}
